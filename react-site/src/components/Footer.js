84fef010165dfd4a2098dd6346b45f5751ec7796
Zaniah Kirkpatrick <zaniahk@email.sc.edu>
2024-11-09 23:10:04 -0500

    changes

--- react-site/src/components/Footer.js
+++ react-site/src/components/Footer.js
@@ -5,12 +5,12 @@ const Footer = () =>{
         <footer>
             <section className="contact">
                 <div className="location">
-                    <h3>Hours of Operation</h3>
-                    <p>Mon - Fri: 9:00 am - 5:00 pm<br/>Sat - Sun: 11:00 am - 5:00 pm</p>
+                    <h2>Hours of Operation</h2>
+                    <h3>Mon - Fri: 9:00 am - 5:00 pm<br/>Sat - Sun: 11:00 am - 5:00 pm</h3>
                 </div>
                 <div className="location">
-                    <h3>Contact Information</h3>
-                    <p>Burgerpalace@gmail.com<br/>(803) 555-3333</p>
+                    <h2>Contact Information</h2>
+                    <h3>Burgerpalace@gmail.com<br/>(803) 555-3333</h3>
                 </div>
             </section>
         </footer>
